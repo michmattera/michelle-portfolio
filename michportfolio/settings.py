d443f4ca75da3e37daa07cc94c3d057d4e757708
michmattera <michellemattera97@gmail.com>
2024-03-05 21:43:35 +0000

    Add typed effect with javascript

--- michportfolio/settings.py
+++ michportfolio/settings.py
@@ -26,9 +26,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-michmattera-michellepor-6kj5vdfmthx.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-michmattera-michellepor-6kj5vdfmthx.ws-us108.gitpod.io']
 
 
 # Application definition
---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+// main js
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: ["I'm a Full-Stack Developer", 'I Build Websites', "I'm a Back-end Developer", 'I am a data analyst', "I'm a Front-end Developer"],
+        typeSpeed: 120,
+        backSpeed: 50,
+        backDelay: 1500,
+        loop: true
+    };
+
+    var typed = new Typed('#headline', options);
+});
