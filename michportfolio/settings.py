7d4cc96dd17058ac72e166ed96ede644d550145e
michmattera <michellemattera97@gmail.com>
2024-03-14 19:16:10 +0000

    Add first project section, start about section

--- michportfolio/settings.py
+++ michportfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-michmattera-michellepor-6kj5vdfmthx.ws-us108.gitpod.io']
+ALLOWED_HOSTS = ['8000-michmattera-michellepor-6kj5vdfmthx.ws-us110.gitpod.io']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
