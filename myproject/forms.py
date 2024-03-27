243824a222e0ad683657a91eb2f5ae047adf7dc2
michmattera <michellemattera97@gmail.com>
2024-03-27 16:38:47 +0000

    Change form to style inputs

--- myproject/forms.py
+++ myproject/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput, Textarea
 from .models import Contact
-
 
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'style': 'width: 100%; height: 40px;'}),
+            'email': EmailInput(attrs={'class': 'form-control', 'style': 'width: 100%; height: 40px;'}),
+            'subject': TextInput(attrs={'class': 'form-control', 'style': 'width: 100%; height: 40px;'}),
+            'message': Textarea(attrs={'class': 'form-control', 'style': 'width: 100%; height: 200px; resize: vertical;'}),
+        }
