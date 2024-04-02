78cfbdefef176148b705299527c396d8c3cb38e3
michmattera <michellemattera97@gmail.com>
2024-04-02 18:26:21 +0000

    Change env file

--- michportfolio/settings.py
+++ michportfolio/settings.py
@@ -30,13 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['/', '8000-michmattera-michellepor-6kj5vdfmthx.ws-us110.gitpod.io']
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'your_email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'your_email_password'
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
