8d54fe954eecfb254f7e6d5890e7a178a0026606
michmattera <michellemattera97@gmail.com>
2024-04-08 22:08:26 +0000

    Start style for media query 1025

--- myproject/views.py
+++ myproject/views.py
@@ -4,34 +4,6 @@ from django.core.mail import EmailMessage
 import os
 from django.core.mail import send_mail
 
-def Home(request):
-    """
-    Home Page with Contact Form
-    """
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            
-            # Send email
-            message_body = f'''
-            New message:
-            From: {email}
-            Name: {name}
-            Message: {message}
-            '''
-            send_mail(subject, message_body, email, ['michellemattera97@gmail.com'])
-
-            return redirect('success')
-    else:
-        form = ContactForm()
-
-    context = {'form': form}
-    return render(request, "index.html", context)
-
 # def Home(request):
 #     """
 #     Home Page with Contact Form
@@ -44,24 +16,57 @@ def Home(request):
 #             subject = form.cleaned_data['subject']
 #             message = form.cleaned_data['message']
 
-#             subject_line = 'Contact form submission from {}'.format(name)
-            
 #             # Send email
-#             email = EmailMessage(
-#                 subject_line,
-#                 message,
-#                 email,
-#                 [os.getenv('EMAIL_ADDRESS')],
-#                 reply_to=[email]
-#             )
-#             email.send()
-#             form.save()
+#             message_body = f'''
+#             New message:
+#             From: {email}
+#             Name: {name}
+#             Message: {message}
+#             '''
+#             send_mail(subject, message_body, email, [''])
+
 #             return redirect('success')
 #     else:
 #         form = ContactForm()
 
 #     context = {'form': form}
 #     return render(request, "index.html", context)
+
+def Home(request):
+    """
+    Home Page with Contact Form
+    """
+    recipient_email = ''  # Specify recipient email directly
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            subject_line = 'Contact form submission from {}'.format(name)
+            
+            # Send email
+            email_message = EmailMessage(
+                subject_line,
+                message,
+                email,
+                [''],  
+                reply_to=[email]
+            )
+            
+            result = email_message.send()
+            print("Email sending result:", result)
+            form.save()
+            return redirect('success')
+    else:
+        form = ContactForm()
+
+    context = {'form': form}
+    return render(request, "index.html", context)
+
 
 
 def success_view(request):
