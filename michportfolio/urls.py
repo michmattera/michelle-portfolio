6c5e6bf1ba99c56bbf1c1de90c02697714604fda
michmattera <michellemattera97@gmail.com>
2024-04-15 21:13:29 +0000

    Change url of favicon

--- michportfolio/urls.py
+++ michportfolio/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 # Url importing url from other application
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myproject.urls'), name='myproject_urls'),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
 ]
