d34d121a3fd06c2c6d7305ffb2e44aa094dd80f5
michmattera <michellemattera97@gmail.com>
2024-04-17 22:23:22 +0000

    Fix menu issue and about img responsive

--- michportfolio/settings.py
+++ michportfolio/settings.py
@@ -36,7 +36,6 @@ CLOUDINARY_STORAGE = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['8000-michmattera-michellepor-6kj5vdfmthx.ws-us110.gitpod.io', 'https://mich-portfolio-8e04475b7506.herokuapp.com/']
 ALLOWED_HOSTS = ['mich-portfolio-8e04475b7506.herokuapp.com',
                  '8000-michmattera-michellepor-6kj5vdfmthx.ws-us110.gitpod.io',
                  'www.michellemattera.com']
@@ -52,7 +51,7 @@ EMAIL_USE_SSL = False
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-michmattera-michellepor-6kj5vdfmthx.ws-us110.gitpod.io',
                         'https://mich-portfolio-8e04475b7506.herokuapp.com/',
-                        'www.michellemattera.com']
+                        'http://www.michellemattera.com']
 # Application definition
 
 INSTALLED_APPS = [
